930baffbf9bfc912c3548309e0c765d83ed6689c
VaishnaviMoutam <vaishnavi.moutam@jukshio.com>
2024-05-12 11:13:38 +0530

    add new file

--- HISTO.py
+++ HISTO.py
@@ -1,13 +1,21 @@
 import numpy as np
 import cv2 as cv
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt 
+import argparse
+
+a = argparse.ArgumentParser()
+a.add_argument("input",help="enter path")
+a.add_argument("output",help="output path")
+b = a.parse_args()
+input=cv.imread(b.input)
+
  
-img = cv.imread('/home/vaishnavi-moutam/Desktop/v/B.jpg')
-cv.imwrite("/home/vaishnavi-moutam/Desktop/v/a.jpg",img)
-assert img is not None, "file could not be read, check with os.path.exists()"
+#img = cv.imread('/home/vaishnavi-moutam/Desktop/v/B.jpg')
+#cv.imwrite("/home/vaishnavi-moutam/Desktop/v/a.jpg",img)
+assert input is not None, "file could not be read, check with os.path.exists()"
 color = ('b','g','r')
 for i,col in enumerate(color):
- histr = cv.calcHist([img],[i],None,[256],[0,256])
+ histr = cv.calcHist([input],[i],None,[256],[0,256])
  plt.plot(histr,color = col)
  plt.xlim([0,256])
 plt.show()
